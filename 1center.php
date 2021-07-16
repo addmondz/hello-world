d899e13c18179272ebd8a2310eab8cb233005345
Edmond <edmond_wuiming@hotmail.com>
2021-07-16 20:37:01 +0800

    a

---
+++ 1center.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html data-language="english">
+
+<head>
+    <!--Font Awesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+</head>
+
+<style>
+    .box_inner {
+
+        width: 100px;
+        height: 100px;
+        border: 1px solid;
+
+    }
+
+    .box {
+        width: 500px;
+        height: 500px;
+        border: 1px solid;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+
+</head>
+
+<body>
+
+    <div class="box">
+        <div class="box_inner"></div>
+    </div>
+
+</body>
+
+</html>
